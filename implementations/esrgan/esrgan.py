6f0348650a739ff46aeffe378cf896ce02d8c7d3
Ubuntu <ubuntu@ip-172-31-32-20.ap-northeast-2.compute.internal>
2023-04-26 12:22:13 +0000

    20230426 initial commit

--- implementations/esrgan/esrgan.py
+++ implementations/esrgan/esrgan.py
@@ -45,7 +45,7 @@ parser.add_argument("--hr_height", type=int, default=256, help="high res. image 
 parser.add_argument("--hr_width", type=int, default=256, help="high res. image width")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=100, help="interval between saving image samples")
-parser.add_argument("--checkpoint_interval", type=int, default=5000, help="batch interval between model checkpoints")
+parser.add_argument("--checkpoint_interval", type=int, default=500, help="batch interval between model checkpoints")
 parser.add_argument("--residual_blocks", type=int, default=23, help="number of residual blocks in the generator")
 parser.add_argument("--warmup_batches", type=int, default=500, help="number of batches with pixel-wise loss only")
 parser.add_argument("--lambda_adv", type=float, default=5e-3, help="adversarial loss weight")
@@ -54,6 +54,8 @@ opt = parser.parse_args()
 print(opt)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
 
 hr_shape = (opt.hr_height, opt.hr_width)
 
