8ef84f61d8dc21fbba9270b9894713785bc2f9f9
Ubuntu <ubuntu@ip-172-31-32-20.ap-northeast-2.compute.internal>
2023-04-26 13:52:12 +0000

    20230426_v3.0

--- implementations/esrgan/test.py
+++ implementations/esrgan/test.py
@@ -71,7 +71,7 @@ state_dict = torch.load(model_path)
 generator.load_state_dict(state_dict)
 
 # An example input you would normally provide to your model's forward() method.
-example = torch.rand(4, 3, 64, 64).to(device)
+example = torch.rand(1, 3, 64, 64).to(device)
 
 # Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
 traced_script_module = torch.jit.trace(generator, example)
